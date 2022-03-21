23ef7c087a70d5ef77a53c71c9a910eb608ef4b3
Dr.Worm <sky.jackson@icloud.com>
2022-03-20 23:38:04 -0400

    got timer working

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,36 @@ var header = document.getElementById("question-header");
 var startBtn = document.getElementById("start-btn");
 var pEl = document.getElementById("page-text");
 var btnGrid = document.getElementById("btn-grid");
+var timerBox = document.getElementById("timer-box");
+// Dynamically created buttons
+var answerBtns = document.querySelectorAll("button");
 
 header.textContent = "Coding Quizaroni";
 pEl.textContent = "Answer the questions before the timer runs out! Beware, incorrect answers will penalize your score by 10 seconds.";
 startBtn.textContent = "Start Quiz";
+var count = 75;
+timerBox.innerText = "Time: " + count;
 
 // create variables that are undefinied to be used below
 var shuffledQuestions, currentQuestionIndex
+
+// Timer countdown function
+var countdown = function() {
+    var timer = setInterval(function() {
+        if(count === 0) {
+            stopInterval()
+        }
+        timerBox.innerText = "Time: " + count;
+        count--;
+    }, 1000);
+
+    var stopInterval = function() {
+        clearInterval(timer);
+        alert("Oh no! You're out of time!");
+        timerBox.innerText = "Game Over";
+    };
+    
+};
 
 // Quiz Array
 var quiz = [
@@ -59,52 +82,65 @@ var quiz = [
     }
 ];
 
-var showQuestion = function(question) {
-    header.innerText = question.question;
+var showQuestion = function(q) {
+    header.innerText = q.question;
     // for each answer of a question listed in quiz array, create buttons for them
-    question.answers.forEach(answer => {
+    q.answers.forEach(answer => {
     var button = document.createElement('button');
         button.innerText = answer.text;
         button.classList.add('button');
         // if the answer is correct, store the data as correct
         if (answer.correct) {
             button.dataset.correct = answer.correct;
+        } else { (!answer.correct)
+            button.dataset.wrong = !answer.correct;
         }
-        button.addEventListener('click', selectAnswer)
+        button.addEventListener('click', selectAnswer);
         btnGrid.appendChild(button);
 })
 }
 
 // function to proceed to next question
 var nextQuestion = function() {
+    resetState();
     showQuestion(shuffledQuestions[currentQuestionIndex]);
+};
+// reset the buttons for new answers
+var resetState = function() {
+    while (btnGrid.firstChild) {
+        btnGrid.removeChild(btnGrid.firstChild);
+    }
 };
 
 var selectAnswer = function(event) {
+    console.log(event.target);
     var selectedButton = event.target;
     var correct = selectedButton.dataset.correct;
-    checkAnswer(button, button.dataset.correct)
-    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    var wrong = selectedButton.dataset.wrong;
+    if (shuffledQuestions.length > currentQuestionIndex + 1 && correct) {
+        currentQuestionIndex++
         nextQuestion();
-    } else {
+    } else if (shuffledQuestions.length > currentQuestionIndex + 1 && wrong) {
+        timerBox.innerHTML = "Time: " + count - 10;
+        selectedButton.classList.add("wrong");
+    } else  {
         showHighscore();
     }
 };
 
+// End of quiz function to gather high score and store it into local storage
 var showHighscore = function() {
-
-};
-
-var checkAnswer = function(element, correct) {
-    if (correct) {
-        nextQuestion();
-    } else {
-        // subtract 10 seconds from timer
-        nextQuestion();
-    }
+    btnGrid.classList.add("hide");
+    header.innerText = "You've finished!";
+    pEl.classList.remove('hide');
+    // pEl.innerText = "Your final score is" + value + "!";
+    // stop timer
+    // insert field for name for high score
+    // store high score in local storage
 };
 
 
+// function to start quiz and shuffle through the quiz array
 var startQuiz = function() {
     // hide original Start button and page text
     startBtn.classList.add('hide');
@@ -113,20 +149,16 @@ var startQuiz = function() {
    shuffledQuestions = quiz.sort(() => Math.random() - .5);
    // set index to zero to set first question selected by quiz.sort as first question [0]
    currentQuestionIndex = 0;
+   // start timer
+   countdown();
    nextQuestion();
 
 
 // on button click start timer at 75 seconds
 // when button is clicked
-// load h1 as question dynamically and load answer buttons from an array dynamically
-// when button is pushed, load next question/answer array
 };
 
 
-
-
-// how to know if a button is correct?
-// how to know if a button is wrong?
 // how to set conditions on a timer?
 
 startBtn.addEventListener("click", startQuiz);
